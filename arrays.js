76b9f5c8fd827ea427c697969d556423b4795b8b
jayceeannne <eric@example.com>
2023-06-21 18:30:27 -0700

    add a new array method

---
+++ arrays.js
@@ -0,0 +1,8 @@
+var flowerTypes = ["Ranunculus", "Snapdragon", "Canterbury bells", "Poppy"]
+var bundleNum = [4, 8, 10, 25]
+var depositPaid = true 
+
+const animals ["puppies", "kittens", "ducklings"];
+const count = animals.push('joeys');
+console.log(animals);
+// has a similar reaction to 'push' but it is added to the beginning of an array.
