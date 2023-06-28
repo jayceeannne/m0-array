660473de438ea302817332f9d9a14e2a4674ed55
jayceeannne <136057571+jayceeannne@users.noreply.github.com>
2023-06-27 20:48:33 -0700

    add revision

--- arrays.js
+++ arrays.js
@@ -1,8 +1,14 @@
-var flowerTypes = ["Ranunculus", "Snapdragon", "Canterbury bells", "Poppy"]
-var bundleNum = [4, 8, 10, 25]
-var depositPaid = true 
+var flowerTypes = ["Ranunculus", "Snapdragon", "Canterbury bells", "Poppy"];
+var bundleNum = [4, 8, 10, 25];
+var depositPaid = true; 
 
-const animals ["puppies", "kittens", "ducklings"];
+console.log(flowerTypes.pop());
+console.log(bundleNum.unshift(5,6));
+
+// .pop printed the last key value inside the array
+// array.unshift() gives the array a new length 
+
+const animals = ["puppies", "kittens", "ducklings"];
 const count = animals.push('joeys');
 console.log(animals);
-// has a similar reaction to 'push' but it is added to the beginning of an array.
+// has a similar reaction to 'push' but it is added to the beginning of an array..
